2070aae6702bc9a6a81e74c0d8d6a7472bd5e9d1
Young-il Cho <terzeron@gmail.com>
2014-03-22 22:47:15 +0900

    initial commit

---
+++ Test1/Test1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Test1
+//
+//  Created by terzeron on 2014. 3. 22..
+//  Copyright (c) 2014년 terzeron.net. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
